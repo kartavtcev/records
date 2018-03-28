aa5c710d84b0625a54d83796ce6e6936c281d04d
kartavtcev <kartavtcev.sergei@gmail.com>
2018-03-28 05:44:36 +0300

    refactor

--- src/main/scala/TimeRecordsRepository.scala
+++ src/main/scala/TimeRecordsRepository.scala
@@ -5,13 +5,13 @@ import slick.driver.H2Driver.api._
 import scala.concurrent.Future
 
 trait TimeRecordsRepository {
-    def addRecords(records : Seq[TimeRecord]) : Future[Any] // Any is for general return which isn't used.
+    def addRecords(records : Seq[TimeRecord]) : Future[Any] // Any is for general return which isn't used here.
     /*
      If you have a streaming action, you can use db.stream instead of db.run to get a Reactive Streams Publisher instead of a Future.
      This allows data to be streamed asynchronously from the database with any compatible library like Akka Streams.
      Slick itself does not provide a full set of tools for working with streams but it has a .foreach utility method for consuming a stream:
     */
-    def getAllBackdatingRecords : DatabasePublisher[TimeRecord] // emulate big data
+    def getAllBackdatingRecords : DatabasePublisher[TimeRecord] // emulate big data consumption with streaming
     def init : Future[Any]
     def close : Unit
 }
@@ -23,19 +23,17 @@ class TimeRecordRepositoryImpl(val db : Database) extends TimeRecordsRepository 
        (select * from TIMERECORDS T2
           where T2.ID < T1.ID AND T2.TIMESTAMP > T1.TIMESTAMP)""".as[TimeRecord]
 
-  override def addRecords(records: Seq[TimeRecord]): Future[Any] = {
+  override def addRecords(records: Seq[TimeRecord]): Future[Any] =
     db.run(timeRecords ++= records)
-  }
 
-  override def getAllBackdatingRecords: DatabasePublisher[TimeRecord] = { // addressing possibly large volume of data
+  override def getAllBackdatingRecords: DatabasePublisher[TimeRecord] = {
     db.stream(backdatingTimeRecordsQuery
       .transactionally
       .withStatementParameters(fetchSize = 1000))
   }
 
-  override def init: Future[Any] = {
+  override def init: Future[Any] =
     db.run(timeRecords.schema.create)
-  }
 
   override def close: Unit = db.close()
 }
